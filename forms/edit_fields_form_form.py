780c06d4711dfa4da1706051eebfe7b7aa74b66f
Iotwo <cattugla@yandex.ru>
2023-12-10 20:42:20 +0700

    Changed EditFields form UI and Interface.

--- forms/edit_fields_form_form.py
+++ forms/edit_fields_form_form.py
@@ -12,38 +12,41 @@ class EditFieldsFormIf(QWidget, Ui_EditFieldsForm):
         self.parentForm = parent
         self.init_ui()
 
+        self.load_operation_types()
         CONTROLS["env"].log.debug("Форма редактирования служебных полей приложения инициализирована.")
 
         return None
 
     def __new__(cls, *args, **kwargs) -> object:
         instance = super().__new__(cls)
+        instance.selected_op = None
         CONTROLS["env"].log.debug(f"Создан экземпляр #{id(instance)}-{type(instance)}")
         
         return instance
     
 
     def init_ui(self) -> None:
-
+        self.operations_listWidget.itemClicked.connect(self.set_selected_operation)
+        self.op_categories_listWidget.itemClicked.connect(self.load_categories_of_optype)
+        #self.add_new_cat_btn.clicked.connect()
+        #self.rm_selected_cat_btn.clicked.connect()
+        #self.exit_btn.clicked.connect()
         return None
 
-    def load_categories_of_optype(self, cmb_index) -> None:
-        rows = CONTROLS["env"].call_select_cats_of_op(f"{getcwd()}\\sql\\get_cats_of_operation.sql", self.opSelection_cmbBox.currentText())
-        self.cats_table.clearContents()
-        self.cats_table.setRowCount(len(rows))
-        rows = [tuple(map(str, [elem for elem in row])) for row in rows]
-        for i in range(len(rows)):
-            for j in range(len(rows[i])):
-                self.cats_table.setItem(i, j, QTableWidgetItem(rows[i][j]))
+    def load_categories_of_optype(self, op_value) -> None:
+        self.op_categories_listWidget.clear()
+        rows = CONTROLS["env"].call_select_cats_of_op(f"{getcwd()}\\sql\\get_cats_of_operation.sql", self.selected_op)
+        for row in rows:
+            self.op_categories_listWidget.addItem(QListWidgetItem(row[0]))
         return None
 
     def load_operation_types(self) -> None:
+        self.operations_listWidget.clear()
         rows = CONTROLS["env"].call_sql_select_cmd(f"{getcwd()}\\sql\\get_all_operations.sql")
-        self.ops_table.clearContents()
-        self.ops_table.setRowCount(len(rows))
-        rows = [tuple(map(str, [elem for elem in row])) for row in rows]
-        for i in range(len(rows)):
-            for j in range(len(rows[i])):
-                self.ops_table.setItem(i, j, QTableWidgetItem(rows[i][j]))
-                
+        for row in rows:
+            self.operations_listWidget.addItem(QListWidgetItem(row[0]))
+        return None
+    
+    def set_selected_operation(self, selectedItem) -> None:
+        self.selected_op = op_value.text()
         return None
